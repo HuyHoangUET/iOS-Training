5448b67af8fd08ec974555de777fbf0d79cf7915
Phạm Huy Hoàng <35534953+HuyHoangUET@users.noreply.github.com>
2020-10-27 11:35:23 +0700

    Add files via upload

--- Canculator/Canculator/ViewModel.swift
+++ Canculator/Canculator/ViewModel.swift
@@ -15,54 +15,41 @@ class ViewModel {
     private var isHasOperator: Bool = false
     private var calculation: String = ""
     
-    init() {
-    }
-    
-    public func setNum1(num1: Float) {
-        self.num1 = num1
-    }
-    public func getNum1() -> Float {
-        return num1
-    }
-    
-    public func setNum2(num2: Float) {
-        self.num2 = num2
-    }
-    public func getNum2() -> Float {
-        return num2
-    }
-    
     // input
-    public func enterNumeral(_ numeral: Character, _ resultLabel: UILabel) {
+    func enterNumeral(numeral: Int, resultLabel: UILabel) {
         if self.calculation == "=" || resultLabel.text == "Infinity" {
             cancel()
             resultLabel.text = "0"
         }
-        
-        if numeral == "." {
-            var isHasDot = false
-            for character in resultLabel.text! {
-                if character == numeral {
-                    isHasDot = true
-                }
-            }
-            if !isHasDot {
-                resultLabel.text?.append(numeral)
+        if isHasOperator == false && resultLabel.text != "0" {
+            if resultLabel.text == "-0" {
+                resultLabel.text?.remove(at: resultLabel.text?.index(before: resultLabel.text?.endIndex ?? "".endIndex) ?? "".endIndex)
+                resultLabel.text?.append(String(numeral))
+            } else {
+                resultLabel.text?.append(String(numeral))
             }
         } else {
-            if isHasOperator == false && resultLabel.text != "0" {
-                resultLabel.text!.append(numeral)
-            } else {
-                resultLabel.text = String(numeral)
-            }
+            resultLabel.text = String(numeral)
         }
     }
     
-    public func setNumInput(_ resultLbl: UILabel) {
-        self.numInput = (resultLbl.text! as NSString).floatValue
+    func enterDecimol(resultLabel: UILabel) {
+        var isHasDecimol = false
+        for char in resultLabel.text ?? ""{
+            if char == "." {
+                isHasDecimol = true
+            }
+        }
+        if !isHasDecimol {
+            resultLabel.text?.append(".")
+        }
     }
     
-    public func setNumbers(_ num: Float) {
+    func setNumInput(resultLbl: UILabel) {
+        self.numInput = ((resultLbl.text ?? "0") as NSString).floatValue
+    }
+    
+    func setNumbers(num: Float) {
         if isHasOperator {
             self.num2 = numInput
         } else {
@@ -72,19 +59,19 @@ class ViewModel {
     }
     
     // algorithm
-    public func sum() {
+    func sum() {
         num1 += num2
     }
     
-    public func subtract() {
+    func subtract() {
         num1 -= num2
     }
     
-    public func product() {
+    func product() {
         num1 = num1 * num2
     }
     
-    public func quotient(_ resultLabel: UILabel) {
+    func quotient(resultLabel: UILabel) {
         if num2 == 0 {
             resultLabel.text = "Infinity"
         } else {
@@ -92,20 +79,32 @@ class ViewModel {
         }
     }
     
-    public func equal() {
-        calculation = "="
+    func percent() {
+        num1 /= 100
+    }
+    
+    func equal() {
+    }
+    
+    func absoluted(resultLabel: UILabel) {
+        if resultLabel.text?.first == "-" {
+            resultLabel.text?.remove(at: resultLabel.text?.startIndex ?? "".startIndex)
+        } else {
+            resultLabel.text = "-" + (resultLabel.text ?? "")
+        }
+        num1 = 0 - num1
     }
     
     // output
-    public func getResult(_ calculation: String, _ resultLabel: UILabel) {
+    func getResult(calculation: String, resultLabel: UILabel) {
         if isHasOperator == false {
             isHasOperator = true
             self.calculation = calculation
         } else {
             switch self.calculation {
             case "/":
-                quotient(resultLabel)
-            case "*":
+                quotient(resultLabel: resultLabel)
+            case "x":
                 product()
             case "-":
                 subtract()
@@ -118,7 +117,7 @@ class ViewModel {
         }
     }
     
-    public func showResult(_ resultLabel: UILabel) {
+    func showResult(resultLabel: UILabel) {
         let num: Int = Int(num1)
         if resultLabel.text != "Infinity" {
             if num1 == Float(num) {
@@ -131,19 +130,19 @@ class ViewModel {
         }
     }
     
-    public func showCalculation(_ calculationLabel: UILabel) {
+    func showCalculation(calculationLabel: UILabel) {
         calculationLabel.text = self.calculation
     }
     
-    public func clickAnOperatorButton(_ calculation: String, _ resultLabel: UILabel, _ calculationLabel: UILabel) {
-        self.setNumInput(resultLabel)
-        self.setNumbers(numInput)
-        self.getResult(calculation, resultLabel)
-        self.showResult(resultLabel)
-        self.showCalculation(calculationLabel)
+    func clickAnOperatorButton(calculation: String, resultLabel: UILabel, calculationLabel: UILabel) {
+        setNumInput(resultLbl: resultLabel)
+        setNumbers(num: numInput)
+        getResult(calculation: calculation, resultLabel: resultLabel)
+        showResult(resultLabel: resultLabel)
+        showCalculation(calculationLabel: calculationLabel)
     }
     
-    public func cancel() {
+    func cancel() {
         num1 = 0
         num2 = 0
         numInput = 0
