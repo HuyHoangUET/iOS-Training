efc71f3e0663706d29fee8eab0fda5a3dfa2d1f1
hoanghy <huyhoanghy98@gmail.com>
2020-11-17 10:35:00 +0700

    extension Array

--- HitsManager/HitsManager/ViewController.swift
+++ HitsManager/HitsManager/ViewController.swift
@@ -149,3 +149,14 @@ extension ViewController {
         cell?.sizeForDeselectedCell()
     }
 }
+
+// Safe array
+extension Array {
+    public subscript(safeIndex index: Int) -> Element? {
+        guard index >= 0, index < endIndex else {
+            return nil
+        }
+
+        return self[index]
+    }
+}
